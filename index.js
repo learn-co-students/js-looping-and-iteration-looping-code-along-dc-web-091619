86c3622637fedaafcbe19da614bfd773719c243f
Nick <nboober@terpmail.umd.edu>
2019-10-26 15:12:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+let messages = [];
+
+function writeCards(name){
+
+    for(let i=0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+
+    }
+    return messages;
+}
+
+function countDown(number){
+    while(number >=0){
+        console.log(number);
+        number--;
+    }
+}
