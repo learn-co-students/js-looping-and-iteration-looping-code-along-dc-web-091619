d1ff33c9dbd51a7aa392e49fdf732dcb297769e8
nihaprezz <sergio13prez@gmail.com>
2019-10-26 23:29:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ['Ada', 'Brenden', 'Ali'];
+
+function writeCards(arrayNames, eventName) {
+    let messages = []
+    for (let i = 0; i < arrayNames.length; i++) {
+      messages.push(`Thank you, ${arrayNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    let i = number 
+    while ( number > 0 ){
+      console.log(number)
+      number --;
+    }
+  console.log(number)
+}
